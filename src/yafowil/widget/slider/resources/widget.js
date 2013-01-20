4f4b7cf4070134cebf3b574e753d70a43bef9f4e
Robert Niederreiter <office@squarewave.at>
2013-01-20 14:00:42 +0100

    cleanup JS

--- src/yafowil/widget/slider/resources/widget.js
+++ src/yafowil/widget/slider/resources/widget.js
@@ -39,43 +39,52 @@ if (typeof(window.yafowil) == "undefined") yafowil = {};
                 return cb;
             },
 
+            callback: function(event, ui) {
+                var widget = $(event.target).parents('.yafowil_slider');
+                var options = widget.data();
+                var elements = widget.data('slider_elements');
+                if (options.range === true) {
+                    elements.lower_value.attr('value', ui.values[0]);
+                    elements.upper_value.attr('value', ui.values[1]);
+                    elements.lower_display.html(ui.values[0]);
+                    elements.upper_display.html(ui.values[1]);
+                } else {
+                    elements.value.attr('value', ui.value);
+                    elements.display.html(ui.value);
+                }
+            },
+
             binder: function(context) {
                 $('.yafowil_slider').each(function() {
                     var widget = $(this);
                     var input = $('input.slider_value', widget);
                     var slider_elem = $('div.slider', widget);
                     var options = widget.data();
-                    var value, lower_value, upper_value;
+                    var elements = {};
+                    widget.data('slider_elements', elements);
                     if (options.range === true) {
-                        lower_value = $('input.lower_value', widget);
-                        upper_value = $('input.upper_value', widget);
-                        options.values = [lower_value.val(), upper_value.val()];
+                        elements.lower_display = $('span.lower_value', widget);
+                        elements.upper_display = $('span.upper_value', widget);
+                        elements.lower_value = $('input.lower_value', widget);
+                        elements.upper_value = $('input.upper_value', widget);
+                        options.values = [elements.lower_value.val(),
+                                          elements.upper_value.val()];
                     } else {
-                        value = $('input.slider_value', widget);
-                        options.value = value.val();
+                        elements.display = $('span.slider_value', widget);
+                        elements.value = $('input.slider_value', widget);
+                        options.value = elements.value.val();
                     }
-                    var callback = function(event, ui) {
-                        if (options.range === true) {
-                            lower_value.attr('value', ui.values[0]);
-                            upper_value.attr('value', ui.values[1]);
-                            $('span.lower_value', widget).html(ui.values[0]);
-                            $('span.upper_value', widget).html(ui.values[1]);
-                        } else {
-                            value.attr('value', ui.value);
-                            $('span.slider_value', widget).html(ui.value);
-                        }
-                    };
                     if (options.slide) {
                         var path = options.slide;
                         options.slide = yafowil.slider.lookup_callback(path);
                     } else {
-                        options.slide = callback;
+                        options.slide = yafowil.slider.callback;
                     }
                     if (options.change) {
                         var path = options.change;
                         options.change = yafowil.slider.lookup_callback(path);
                     } else {
-                        options.change = callback;
+                        options.change = yafowil.slider.callback;
                     }
                     slider_elem.slider(options);
                 });
