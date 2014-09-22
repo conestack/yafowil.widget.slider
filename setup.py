2fa9cdb309fea9cefebc8b103c1b9fc48ddcd4d2
Robert Niederreiter <office@squarewave.at>
2014-09-22 10:01:18 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 )
 
 
-version = '1.1pre1'
+version = '1.1.dev1'
 shortdesc = 'Slider Widget for YAFOWIL'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
