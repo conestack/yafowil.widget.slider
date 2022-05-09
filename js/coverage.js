5752db10f696375b6b6c026b6addffdd8c2f0db5
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-05-09 13:48:05 +0200

    npm script update

--- js/coverage.js
+++ js/coverage.js
@@ -1,4 +1,14 @@
-console.log('coverage here!');
-
+const percentage = {
+  lines: 100,
+  statements: 100,
+  functions: 100,
+  branches: 100
+}
 var some = require('./karma/coverage/coverage-summary.json');
-console.log(some)
+
+for (let res in some.total) {
+  if (some.total[res].pct < percentage[res]) {
+    console.log(`Coverage too low, expected: ${percentage[res]}, got: ${some.total[res].pct}`);
+    throw new Error(`Coverage too low, expected: ${percentage[res]}, got: ${some.total[res].pct}`);
+  }
+}
