71289a2c32c07a44aa69046183fddf7f052a4bef
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 13:34:37 +0200

    "Karma coverage validation script formatting"

--- js/karma-coverage.js
+++ js/karma-coverage.js
@@ -9,9 +9,9 @@ var summary = require('./karma/coverage/coverage-summary.json');
 for (let res in summary.total) {
     if (summary.total[res].pct < percentage[res]) {
         throw new Error(
-        `Coverage too low on ${res},
-        expected: ${percentage[res]},
-        got: ${summary.total[res].pct}`
+            `Coverage too low on ${res},
+            expected: ${percentage[res]},
+            got: ${summary.total[res].pct}`
         );
     }
 }
